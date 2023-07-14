9d549161dfe7997cc3bd8fd6b46ae647f14fb3d8
fl0rest <120335796+fl0rest@users.noreply.github.com>
2023-07-14 08:34:04 -0400

    Update mxtb.sh

--- bin/mxtb.sh
+++ bin/mxtb.sh
@@ -6,3 +6,8 @@ if [[ $search =~ .[a-z] ]]; then
 else
         echo 'https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a'$search'&run=toolpage'
 fi
+echo 'https://multirbl.valli.org/lookup/'$search
+
+echo "Results from multirbl:"
+echo "Not listed: "$(curl -sL multirbl.valli.org/lookup/$search | grep nof | grep -v nofollow | cut -d: -f 2 | rev | cut -c 2- | rev | awk '{ sum += $1; } END { print sum; }')
+echo "Listed on: "$(curl -sL multirbl.valli.org/lookup/$search | grep Blacklist | grep -v nofollow | cut -d: -f 2 | rev | cut -c 2- | rev | awk '{ sum += $1; } END { print sum; }')
