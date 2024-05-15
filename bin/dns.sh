dc828a33cd2ed4f62dcb3037f8413bb450a1db31
fl0rest <120335796+fl0rest@users.noreply.github.com>
2024-05-15 18:11:24 -0400

    Update dns.sh

--- bin/dns.sh
+++ bin/dns.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 DOMAIN=${1}
-echo "Name servers: " ; dig @8.8.8.8 ns $DOMAIN +short
-echo "A record: "
-dig @8.8.8.8 a $DOMAIN +short
-echo "WWW record: "
-dig @8.8.8.8 www.$DOMAIN +short
-echo "rDNS: "
-dig @8.8.8.8 -x $(dig @8.8.8.8 $DOMAIN +short) +short
-echo "MX records: "
-dig @8.8.8.8 mx $DOMAIN +short ; dig @8.8.8.8 $(dig @8.8.8.8 mx $DOMAIN +short) +short
-echo "SPF, DKIM & DMARC"
+echo "DNS Zone"
+echo -n "NS: " 
+dig @8.8.8.8 ns $DOMAIN +short | awk '{printf "%s ",$0 } END {print ""}'
+echo -n "A: "
+dig @8.8.8.8 $DOMAIN +short | awk '{printf "%s ",$0} END {print ""}'
+echo -n "WWW: "
+dig @8.8.8.8 www.$DOMAIN +short | awk '{printf "%s ",$0} END {print ""}'
+echo -n "rDNS: "
+dig @8.8.8.8 -x $(dig @8.8.8.8 $DOMAIN +short) +short | awk '{printf "%s ",$0} END {print ""}'
+echo -n "MX: "
+dig @8.8.8.8 mx $DOMAIN +short ; dig @8.8.8.8 $(dig @8.8.8.8 mx $DOMAIN +short) +short | awk '{printf "%s ",$0} END {print ""}'
+echo "-----SPF and DKIM-----"
 dig @8.8.8.8 txt $DOMAIN +short
 dig @8.8.8.8 txt default._domainkey.$DOMAIN +short
 dig @8.8.8.8 txt _dmarc.$DOMAIN +short
+echo
+
+whois $DOMAIN | grep -i 'registrar url' | head -1
